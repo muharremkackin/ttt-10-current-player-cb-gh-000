9919d5fa2ad89d590a137910ca9a67ca479e1ed8
Muharrem Kaçkın <muharremkackin@gmail.com>
2018-05-15 14:02:14 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,26 @@
-def turn_count(board)
-  counter = 0
-  board.each do |position|
-    if position == "X" || position == "O"
-      counter += 1
-    end
-  end
-  counter
-end
+# def turn_count(board)
+#   counter = 0
+#   board.each do |position|
+#     if position == "X" || position == "O"
+#       counter += 1
+#     end
+#   end
+#   counter
+# end
+# 
+# board = [" ", "X", "O", " ", "X", " ", "X ", " ", "O"]
+# 
+# def current_player(board)
+#   turn_count(board).even? ? "X" : "O"
+# end
 
-board = [" ", "X", "O", " ", "X", " ", "X ", " ", "O"]
+students = [
+  ["Bruce Banner", "10 Grade", "A+ Average"]
+  ["Tony Stark", "12 Grade", "A+ Average"]
+  ["Thor", "9 Grade", "C Average"]
+]
 
-def current_player(board)
-  turn_count(board).even? ? "X" : "O"
+students.each do |student|
+  puts student
+
 end
